1fc1c38d2f0cafff12b6de8d6d01aaa125485ce8
Josh Field <10372036+HexaField@users.noreply.github.com>
2025-03-01 16:08:09 +1100

    Initial commit

---
+++ src/CustomLocationPage.tsx
@@ -0,0 +1,102 @@
+import '@ir-engine/client/src/engine'
+
+import {
+  EntityTreeComponent,
+  UndefinedEntity,
+  createEntity,
+  defineSystem,
+  getComponent,
+  setComponent
+} from '@ir-engine/ecs'
+import { ECSState } from '@ir-engine/ecs/src/ECSState'
+import { Engine } from '@ir-engine/ecs/src/Engine'
+import {
+  defineState,
+  getMutableState,
+  getState,
+  useImmediateEffect,
+  useMutableState,
+  useReactiveRef
+} from '@ir-engine/hyperflux'
+import { ReferenceSpaceState } from '@ir-engine/spatial'
+import { CameraComponent } from '@ir-engine/spatial/src/camera/components/CameraComponent'
+import { NameComponent } from '@ir-engine/spatial/src/common/NameComponent'
+import { Vector3_Up } from '@ir-engine/spatial/src/common/constants/MathConstants'
+import { destroySpatialEngine, initializeSpatialEngine } from '@ir-engine/spatial/src/initializeEngine'
+import { MeshComponent } from '@ir-engine/spatial/src/renderer/components/MeshComponent'
+import { VisibleComponent } from '@ir-engine/spatial/src/renderer/components/VisibleComponent'
+import { useEngineCanvas } from '@ir-engine/spatial/src/renderer/functions/useEngineCanvas'
+import { TransformComponent } from '@ir-engine/spatial/src/transform/components/TransformComponent'
+import { TransformSystem, computeTransformMatrix } from '@ir-engine/spatial/src/transform/systems/TransformSystem'
+
+import React, { useEffect } from 'react'
+import { BoxGeometry, Mesh, MeshBasicMaterial } from 'three'
+
+const SceneState = defineState({
+  name: 'ee.minimalist.SceneState',
+  initial: {
+    entity: UndefinedEntity
+  }
+})
+
+const UpdateSystem = defineSystem({
+  uuid: 'ee.minimalist.UpdateSystem',
+  insert: { before: TransformSystem },
+  execute: () => {
+    const entity = getState(SceneState).entity
+    if (!entity) return
+
+    const elapsedSeconds = getState(ECSState).elapsedSeconds
+    const transformComponent = getComponent(entity, TransformComponent)
+    transformComponent.rotation.setFromAxisAngle(Vector3_Up, elapsedSeconds)
+  },
+  reactor: function () {
+    const viewerEntity = useMutableState(ReferenceSpaceState).viewerEntity.value
+
+    useEffect(() => {
+      if (!viewerEntity) return
+
+      // Create a new entity
+      const entity = createEntity()
+      setComponent(entity, TransformComponent)
+      setComponent(entity, EntityTreeComponent, { parentEntity: Engine.instance.originEntity })
+
+      // Create a box at the origin
+      const mesh = new Mesh(new BoxGeometry(1, 1, 1), new MeshBasicMaterial({ color: 0x00ff00 }))
+      setComponent(entity, MeshComponent, mesh)
+      setComponent(entity, NameComponent, 'Box')
+      setComponent(entity, VisibleComponent)
+
+      // Make the camera look at the box
+      const cameraTransform = getComponent(viewerEntity, TransformComponent)
+      const camera = getComponent(viewerEntity, CameraComponent)
+      cameraTransform.position.set(5, 2, 0)
+      cameraTransform.rotation.copy(camera.quaternion)
+      computeTransformMatrix(viewerEntity)
+      camera.lookAt(0, 0, 0)
+
+      getMutableState(SceneState).entity.set(entity)
+    }, [viewerEntity])
+
+    return null
+  }
+})
+
+export default function Template() {
+  const [ref, setRef] = useReactiveRef()
+
+  useImmediateEffect(() => {
+    initializeSpatialEngine()
+    return () => {
+      destroySpatialEngine()
+    }
+  }, [])
+
+  useEngineCanvas(ref)
+
+  return (
+    <>
+      <div ref={setRef} style={{ width: '100%', height: '100%' }} />
+    </>
+  )
+}
